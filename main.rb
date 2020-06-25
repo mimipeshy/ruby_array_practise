d7bac457afb9e9bc9f05919948827a22e3f16706
peris ndanu <perisndanu@gmail.com>
2020-06-25 22:04:53 +0000

    solved simple array iteration

---
+++ main.rb
@@ -0,0 +1,5 @@
+ #Use the each method of Array to iterate over [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], and print out each value.
+
+array=[1,2,3,4,5,6,7,8,9,10]
+
+array.each {|number| puts "the value is #{number}"}
